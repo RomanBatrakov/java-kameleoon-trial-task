19c7b038f8549cfb25f194b23ba4834c255e3d8e
Roman_B <batrakov.r.v@yandex.ru>
2023-02-09 10:17:55 +0300

    first commit

---
+++ src/main/java/com/kameleoon/bett/JavaKameleoonTrialTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kameleoon.bett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKameleoonTrialTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKameleoonTrialTaskApplication.class, args);
+    }
+
+}
